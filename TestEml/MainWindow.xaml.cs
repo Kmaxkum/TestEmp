91fc231314deaacd6c5ebb2569dcf8dfd8504ee3
Максим Крутов <Kmaxkum20@gmail.com>
2019-09-18 14:26:53 +0400

    Добавьте файлы проекта.

---
+++ TestEml/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using RestSharp;
+using System.Windows;
+
+namespace TestEml
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string host = "http://localhost:8080/";
+        private string path = "Loks/1";
+        private Pack pack = new Pack();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            pack.Token = "qwe";
+            pack.LockStatus = true;
+            pack.PowerStatus = false;
+            pack.NetStatus = true;
+            Update();
+        }
+
+        private void Button_Click_Lock(object sender, RoutedEventArgs e)
+        {
+            pack.LockStatus = !pack.LockStatus;
+            Update();
+        }
+
+        private void Button_Click_Power(object sender, RoutedEventArgs e)
+        {
+            pack.PowerStatus = !pack.PowerStatus;
+            Update();
+        }
+
+        private void Button_Click_Net(object sender, RoutedEventArgs e)
+        {
+            pack.NetStatus = !pack.NetStatus;
+            Update();
+        }
+
+        private void Button_Click_Request(object sender, RoutedEventArgs e)
+        {
+            var client = new RestClient(host);
+            var request = new RestRequest(path, Method.POST);
+            request.AddJsonBody(pack);
+            var ans = client.Execute(request);
+            textBlockReq.Text = ans.ToString();
+            Update();
+        }
+
+        private void Update() {
+            textBlockLock.Text = pack.LockStatus.ToString();
+            textBlockPower.Text = pack.PowerStatus.ToString();
+            textBlockNet.Text = pack.NetStatus.ToString();
+        }
+    }
+}
---
+++ TestEml/Pack.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TestEml
+{
+    class Pack
+    {
+        [JsonProperty("Token")]
+        public string Token { get; set; }
+
+        [JsonProperty("LockStatus")]
+        public bool LockStatus { get; set; }
+
+        [JsonProperty("PowerStatus")]
+        public bool PowerStatus { get; set; }
+
+        [JsonProperty("NetStatus")]
+        public bool NetStatus { get; set; }
+    }
+}
